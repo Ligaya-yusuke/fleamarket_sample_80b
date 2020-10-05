5b4123799dffa68172fd30d936373c811ca1cff2
Ligaya-yusuke <e98069@gmail.com>
2020-10-05 13:15:44 +0900

    change-appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,18 @@
 class ApplicationController < ActionController::Base
-before_action :basic_auth
-
-private
-
-def basic_auth
-  authenticate_or_request_with_http_basic do |username, password|
-    username == Rails.application.credentials[:basic_auth][:user] &&
-    password == Rails.application.credentials[:basic_auth][:pass]
-  end
-end
-
-# if: :production?を追加
   before_action :basic_auth, if: :production?
 
-private
-# 以下を追記
-def production?
-  Rails.env.production?
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
+
+  private
+  # 以下を追記
+  def production?
+    Rails.env.production?
+  end
 end
