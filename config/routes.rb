dce8fe5170742b108ff80fa3604120104d1061a9
Ligaya-yusuke <e98069@gmail.com>
2020-10-24 18:58:03 +0900

    corrected-codeReviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  # トップページへのリンク
   # root 'items#index'
   root to: 'sells#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
