d7ef8973bd353956b05801cd6494af58ba627c76
Shouma63 <choma63.twins24@gmail.com>
2020-10-25 12:21:53 +0900

    migration commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_034848) do
+ActiveRecord::Schema.define(version: 2020_10_25_031814) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ship_first_name", null: false
@@ -21,12 +21,31 @@ ActiveRecord::Schema.define(version: 2020_10_20_034848) do
     t.string "prefecture", null: false
     t.string "city", null: false
     t.string "block_number", null: false
-    t.string "building_number"
+    t.string "building_number", default: ""
     t.string "phone_number", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "prefecture_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "information", null: false
+    t.string "price", null: false
+    t.string "brand"
+    t.string "condition", null: false
+    t.string "delivery_change", null: false
+    t.string "shipping_day", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -58,22 +77,4 @@ ActiveRecord::Schema.define(version: 2020_10_20_034848) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "profiles", "users"
-  
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "information", null: false
-    t.string "price", null: false
-    t.string "brand"
-    t.string "condition", null: false
-    t.string "delivery_change", null: false
-    t.string "shipping_day", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 end
