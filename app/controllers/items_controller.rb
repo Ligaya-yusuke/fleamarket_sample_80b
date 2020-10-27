941f72e53097c4a7cb6cdf37f7eabd28bf33fbec
swakawis <62795565+swakawis@users.noreply.github.com>
2020-10-27 23:15:06 +0900

    detail-page1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
+
+  add_breadcrumb 'FURIMA', '/'
+  add_breadcrumb 'レディース', '/'
+  add_breadcrumb 'トップス', '/'
+  add_breadcrumb 'Tシャツ/カットソー(半袖/袖なし)', '/'
+  add_breadcrumb 'テスト'
+
   def index
   end
+
+  def show
+
+  end
+
 end
