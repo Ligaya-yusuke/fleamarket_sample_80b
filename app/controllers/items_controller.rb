dbd4adc73b8ddb758dd9baa0e0e61f8e21e1e0f4
swakawis <62795565+swakawis@users.noreply.github.com>
2020-10-30 13:19:57 +0900

    detail-view
    the view of products detail page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,5 @@
 class ItemsController < ApplicationController
 
-  add_breadcrumb 'FURIMA', '/'
-  add_breadcrumb 'レディース', '/'
-  add_breadcrumb 'トップス', '/'
-  add_breadcrumb 'Tシャツ/カットソー(半袖/袖なし)', '/'
-  add_breadcrumb 'テスト'
-
   def index
   end
 
