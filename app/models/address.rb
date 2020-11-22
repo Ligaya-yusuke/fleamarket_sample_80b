0df112efa956992c415f94e49a04d86e41fc4bf9
Shouma63 <choma63.twins24@gmail.com>
2020-11-22 17:37:08 +0900

    validation-fix

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,10 +2,20 @@ class Address < ApplicationRecord
 
   belongs_to :user, optional: true
   validates :ship_first_name, :ship_family_name, :ship_first_name_kana, :ship_family_name_kana, :post_code, :prefecture_id, :city, :block_number, :phone_number , presence: true
+  validates :ship_first_name, :ship_family_name,
+                        format: {
+                          with: /\A[ぁ-んァ-ン一-龥]/,
+                          message: "を全角文字で入力してください"
+                        }
   validates :ship_first_name_kana, :ship_family_name_kana,
                         format: {
                           with: /\A[ぁ-んー－]+\z/,
-                          message: "全角文字で入力してください"
+                          message: "を全角文字で入力してください"
+                        }
+  validates :post_code,
+                        format: {
+                          with: /\A\d{3}[-]\d{4}\z/,
+                          message: "(3桁-4桁)" 
                         }
 
   # jp_prefecture :prefecture_code
