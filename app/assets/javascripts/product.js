13418083ac64fa5909799a4ae318146c369e7784
12-8078mk <6c787b.itm.744a@gmail.com>
2020-11-27 20:59:14 +0900

    delete #words

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -7,7 +7,7 @@ function appendOption(category) {
 function appendChildrenBox(insertHTML) {
   let childSelectHtml = '';
   childSelectHtml = 
-    `<select class="item_input__body__category__children--select" id="children_category" required="true">
+    `<select class="item_input__body__category__children--select" id="children_category">
        <option value="" data-category="" >選択してください</option>
        ${insertHTML}</select>
      <i class = "fa fa-chevron-down"></i>`;
@@ -17,7 +17,7 @@ function appendChildrenBox(insertHTML) {
 function appendGrandchildrenBox(insertHTML) {
   let grandchildSelectHtml = '';
   grandchildSelectHtml = 
-    `<select class="item_input__body__category__grandchildren--select" id="grandchildren_category" name="product[category_id]" required="true">
+    `<select class="item_input__body__category__grandchildren--select" id="grandchildren_category" name="product[category_id]">
        <option value="" data-category="" >選択してください</option>
        ${insertHTML}</select>
      `;
