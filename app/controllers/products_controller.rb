7202ccf0b577db4f8a553e709ad4a3fd4bb66214
12-8078mk <6c787b.itm.744a@gmail.com>
2020-11-28 17:45:32 +0900

    images show OK

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,7 @@ class ProductsController < ApplicationController
 
   def show
     @image_first = Image.where(product_id: @product).first.src.url
-    @images = Image.where(product_id: @product)
+    @images = Image.where(product_id: @product).limit(4)
     @category = Category.find(@product.category_id)
     @user = User.find(@product.user_id)
     @address = Prefecture.find(@product.prefecture_id)
