bec9c35a3a605edd8defa3ddf39ff044c445b986
Ligaya-yusuke <e98069@gmail.com>
2020-11-29 14:08:04 +0900

    refactoring1

--- app/models/product.rb
+++ app/models/product.rb
@@ -21,12 +21,6 @@ class Product < ApplicationRecord
   # has_many :likes, dependent: :destroy
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-
-
-
-
-
-
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_charge
   belongs_to_active_hash :shipping_day
