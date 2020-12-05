0b59e448a9e27e906fc17b7450d58473adaeec37
Shouma63 <choma63.twins24@gmail.com>
2020-12-05 17:05:25 +0900

    kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @item = Product.where(user_id: current_user.id)
     @item = @item.all.order(id: "DESC")
+    @item = @item.page(params[:page]).per(9)
+    #  json形式にする時に使用
+      # respond_to do |format|
+      #   format.html
+      #   format.js
+      # end
     @count = Product.where(user_id: current_user.id).count
   end
 
