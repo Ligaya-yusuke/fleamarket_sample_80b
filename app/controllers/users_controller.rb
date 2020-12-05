539237dd0857e7abc8b4b901450cc5fc76f254c1
Shouma63 <choma63.twins24@gmail.com>
2020-12-05 12:00:36 +0900

    mypage-iamge-count-desc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @item = Product.where(user_id: current_user.id)
+    @item = @item.all.order(id: "DESC")
+    @count = Product.where(user_id: current_user.id).count
   end
 
 
