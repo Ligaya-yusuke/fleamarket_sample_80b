657a3c84070bdb0e9df22e5a6899e3c1221ac4fe
Ligaya-yusuke <e98069@gmail.com>
2020-12-12 12:44:34 +0900

    show-dash-lessthan300

--- app/assets/javascripts/calculation.js
+++ app/assets/javascripts/calculation.js
@@ -1,5 +1,5 @@
 // 販売価格を入力し、販売手数料と販売利益を計算する関数宣言
-$(function(){
+$(function (){
   
   $('#product_price').on('input',function(){
     // 販売価格の入力欄に入力すると反応
@@ -9,7 +9,7 @@ $(function(){
       let fee=Math.ceil(sales * 0.1);
       let profit=Math.floor(sales * 0.9);
       if (sales<300) {
-        console.log('➖');
+        $('.Products__form__fees__rate').html('➖') && $('.Products__form__profits__amount').html('➖');;
       } else{
         $('.Products__form__fees__rate').html(fee) && $('.Products__form__profits__amount').html(profit);
       }
